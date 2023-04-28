dad183c6b15ff8ae4a69471c57f265df1bb67659
Mathioussee <58639269+Mathiousse@users.noreply.github.com>
2023-04-28 16:31:07 +0200

    Co-authored-by: YuMs00 <YuMs00@users.noreply.github.com> Co-authored-by: FabueroTM <FabueroTM@users.noreply.github.com>

--- Back/sneak-back/app/Http/Controllers/ResponseController.php
+++ Back/sneak-back/app/Http/Controllers/ResponseController.php
@@ -23,16 +23,33 @@ class ResponseController extends Controller
      */
     public function create()
     {
-        //
+        $keywords = Keyword::all();
+        $responses = Response::all();
+        return view('create/response', compact('keywords'), compact('responses'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
+{
+    $request->validate([
+        
+        'message' => 'required|string',
+        'keywords' => 'array',
+        'type' => 'required|string',
+        
+    ]);
+
+    $response = Response::create($request->all());
+    $keywords = [$request->keywords];
+    if (isset($request->keywords)) {
+        $response->keywords()->attach($keywords);
     }
+    // get the keywords from request->keyword, loop on it, find the keyword with the good ID and use attach() to
+
+    return redirect()->route('response');
+}
 
     /**
      * Display the specified resource.
@@ -47,15 +64,33 @@ class ResponseController extends Controller
      */
     public function edit(Response $response)
     {
-        //
+        $keywords = Keyword::all();
+        $selectedKeywords = $response->keywords->pluck('id')->toArray();
+    
+        return view('edits/response', compact('response', 'keywords', 'selectedKeywords'));
     }
+    
+
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Response $response)
     {
-        //
+        $this->validate($request, [
+            'message' => 'required|string',
+            'keywords' => 'array',
+            'type' => 'required|string',
+        ]);
+    
+        $response->update([
+            'message' => $request->input('message'),
+            'keyword' => $request->input('keyword'),
+            'type' => $request->input('type'),
+            
+        ]);
+    
+        return redirect()->route('response')->with('success', 'L\'utilisateur a été modifié avec succès.');
     }
 
     /**
@@ -63,6 +98,9 @@ class ResponseController extends Controller
      */
     public function destroy(Response $response)
     {
-        //
+        $response->delete();
+
+    return redirect()->route('response')
+        ->with('success', 'Le produit a été supprimé avec succès.');
     }
 }
